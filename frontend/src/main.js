bf3ece55108935f32f7b440073bf7a01fa0567e5
RomeoChen <chen.zitong@qq.com>
2020-05-23 11:39:03 +0800

    添加vue-router插件

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import './style/app.css';
+import router from './router'
 
 Vue.config.productionTip = false;
 
@@ -13,5 +14,6 @@ window.env = {}
 window.env.apiUrl = 'https://service-g9p31ojq-1257634896.gz.apigw.tencentcs.com/release/'
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app');
---
+++ frontend/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Loupan from '../views/Loupan.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Loupan',
+    component: Loupan
+  },
+  {
+    path: '/bankuai',
+    name: 'Bankuai',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Bankuai.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
