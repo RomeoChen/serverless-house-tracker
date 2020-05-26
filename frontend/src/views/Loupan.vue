4c31685692b928893cea2f1a045950202ca1fbc5
RomeoChen <chen.zitong@qq.com>
2020-05-26 18:17:58 +0800

    add: 请求加载

--- frontend/src/views/Loupan.vue
+++ frontend/src/views/Loupan.vue
@@ -2,13 +2,13 @@
   <div class="loupan-container">
     <a-menu mode="horizontal" :default-selected-keys="['search']">
       <a-menu-item key="search">
-        <router-link to="/">搜索</router-link>
+        <router-link to="/">楼盘趋势</router-link>
       </a-menu-item>
       <a-menu-item key="linkList">
-        <router-link to="/link-list">链接列表</router-link>
+        <router-link to="/link-list">楼盘链接</router-link>
       </a-menu-item>
       <a-menu-item key="sort">
-        <router-link to="/sort">排序</router-link>
+        <router-link to="/sort">楼盘变化率</router-link>
       </a-menu-item>
     </a-menu>
     <div class="loupan-content">
@@ -22,6 +22,9 @@ export default {
   data() {
     return {
     }
+  },
+  mounted() {
+    this.$store.dispatch('getUrlList');
   }
 }
 </script>
